82186f5124a691e716b163e3fdb277c16675b716
NK Studio <53283888+NK-Studio@users.noreply.github.com>
2024-01-25 15:17:51 +0900

    UnityPlayerGameActivity Update

--- Assets/Plugins/Android/UPlugin.kt
+++ Assets/Plugins/Android/UPlugin.kt
@@ -3,32 +3,36 @@ package com.unity3d.player
 import android.app.AlertDialog
 import android.os.VibrationEffect
 import android.os.Vibrator
+import android.util.Log
 
-import com.unity3d.player.UnityPlayer
-import com.unity3d.player.UnityPlayerActivity
+class UPlugin {
 
-class UPlugin : UnityPlayerActivity()
-{
-    private fun ShowDialogConfirm(title : String, message : String, yes : String, no: String, cancelable : Boolean)
-    {
+    private fun ShowDialogConfirm(
+            title: String,
+            message: String,
+            yes: String,
+            no: String,
+            cancelable: Boolean
+    ) {
         AlertDialog.Builder(UnityPlayer.currentActivity)
-            .setTitle(title)
-            .setMessage(message)
-            .setPositiveButton(yes) { _, _ ->
-                UnityPlayer.UnitySendMessage("MobileDialogConfirm", "OnYesCallBack", "0");
-            }
-            .setNegativeButton(no) { _, _ ->
-                UnityPlayer.UnitySendMessage("MobileDialogConfirm", "OnNoCallBack", "1");
-            }
-            .setCancelable(cancelable)
-            .create()
-            .show()
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton(yes) { _, _ ->
+                    UnityPlayer.UnitySendMessage("MobileDialogConfirm", "OnYesCallBack", "0");
+                }
+                .setNegativeButton(no) { _, _ ->
+                    UnityPlayer.UnitySendMessage("MobileDialogConfirm", "OnNoCallBack", "1");
+                }
+                .setCancelable(cancelable)
+                .create()
+                .show()
     }
 
     // 진동 호출
-    private fun Vibrate(milliseconds : Long, amplitude : Int)
-    {
-        val vibrator = UnityPlayer.currentActivity.getSystemService(VIBRATOR_SERVICE) as Vibrator
+    private fun Vibrate(milliseconds: Long, amplitude: Int) {
+
+        val vibrator =
+                UnityPlayer.currentActivity.getSystemService(UnityPlayerGameActivity.VIBRATOR_SERVICE) as Vibrator
         vibrator.vibrate(VibrationEffect.createOneShot(milliseconds, amplitude));
     }
 }
